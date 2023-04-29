20c14f62f266ff6a2d34f1709ac31a6de5934bd4
hansfigo <hambasahaya0303@gmail.com>
2023-04-30 02:45:05 +0700

    Add : Scrollbar Styling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
 	theme: {
 		extend: {},
 	},
-	plugins: [require("daisyui"), require('@tailwindcss/typography')],
+	plugins: [require("daisyui"), require('@tailwindcss/typography'), require('tailwind-scrollbar'),
+	],
 }
