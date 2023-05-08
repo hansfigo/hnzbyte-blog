b6e6cefd8efb3313c33d25db65d4fc24b6d75b9a
hansfigo <hambasahaya0303@gmail.com>
2023-05-08 21:51:15 +0700

    feat: add support for Storyblok CMS integration
    - Add `@storyblok/astro` package to `package.json`
    - Add `storyblok` integration to `astro.config.mjs`
    - Add `useStoryblokApi` hook to fetch data from Storyblok CMS
    - Add `BlogPost` and `BlogPostList` components to render blog posts from Storyblok CMS
    - Update `MainHead` and `MainLayout` components to include Open Graph metadata
    - Update `index.astro` and `BlogPost.astro` to use `MainLayout` component
    - Update `index.astro` to use `BlogPostList` component to render blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from 'vite';
 
 import svelte from "@astrojs/svelte";
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte(), storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      blogPost: 'storyblok/BlogPost',
+    },
+  })]
 });
