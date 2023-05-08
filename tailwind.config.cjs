1baf01c7404342409b31c4968fc7367dfb70501e
hansfigo <hambasahaya0303@gmail.com>
2023-05-09 01:14:04 +0700

    Add : Syntax Highlight and Code Tag Style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,33 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			typography: {
+				DEFAULT: {
+					css: {
+						// color: '#333',
+						// a: {
+						// 	color: '#3182ce',
+						// 	'&:hover': {
+						// 		color: '#2c5282',
+						// 	},
+						// },
+						pre: {
+							'code.hljs': {
+								'overflow-x': 'auto',
+								'padding' : '20px',
+								'&::-webkit-scrollbar': {
+									'height': '4px',
+								},
+								'&::-webkit-scrollbar-thumb': {
+									background: 'hsl(var(--p))',
+								  }
+							}
+						}
+					},
+				},
+			},
+		}
 	},
 	plugins: [require("daisyui"), require('@tailwindcss/typography'), require('tailwind-scrollbar'),
 	],
